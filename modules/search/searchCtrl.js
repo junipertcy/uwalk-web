61f2a55f426cea6d95030d8406368c31dad20331
Tzu-Chi Yen <junipe@Tzu-Chis-Basecamp.local>
2016-07-10 20:21:55 +0800

    add fake search result and about page (inside airbnb's template)

--- modules/search/searchCtrl.js
+++ modules/search/searchCtrl.js
@@ -25,13 +25,16 @@ function Index(indexService, $scope, $timeout, $mdSidenav, $log) {
       zoom: 11
     },
     defaults: {
-      zoomControl: false,
+      zoomControl: true,
       doubleClickZoom: false,
       scrollWheelZoom: false,
       tap: false,
       touchZoom: false,
       minZoom: 11,
-      maxZoom: 11
+      maxZoom: 15,
+      attributionControl: false,
+      zoomsliderControl: true,
+      zoomControlPosition: 'bottomleft'
     }
   });
 
@@ -131,6 +134,75 @@ angular.module('simhood').controller('LeftCtrl', ['$scope', '$timeout','$mdSiden
           $log.debug("close RIGHT is done");
         });
     };
+
+    $scope.queryResults = [{
+      hoodId: '',
+      similarity: '9.23',
+      parentCity: 'London',
+      pic: 'assets/img/searchResult/piccadilly_circus.jpg',
+      desc: 'Piccadilly Circus is a road junction and public space of London\'s West End in the City of Westminster, built in 1819 to connect Regent Street with Piccadilly.',
+      data: {
+        beer: 23,
+        cutlery: 34,
+        coffee: 84,
+        bed: 23,
+        car: 10
+      }
+    }, {
+      hoodId: '',
+      similarity: '8.74',
+      parentCity: 'Paris',
+      pic: 'assets/img/searchResult/AdCE.jpg',
+      desc: 'The Avenue des Champs-Élysées is a boulevard in the 8th arrondissement of Paris, 1.9 kilometres long and 70 metres wide, which runs between the Place de la Concorde and the Place Charles de Gaulle, where the Arc de Triomphe is located.',
+      data: {
+        beer: 19,
+        cutlery: 38,
+        coffee: 35,
+        bed: 93,
+        car: 12
+      }
+    }, {
+      hoodId: '',
+      similarity: '8.48',
+      parentCity: 'Beijing',
+      pic: 'assets/img/searchResult/sanlitun.jpg',
+      desc: 'Sanlitun is an area of the Chaoyang District, Beijing containing many popular bar streets and international stores. The area has been under almost constant regeneration since the late 20th century as part of a city-wide project of economic regrowth.',
+      data: {
+        beer: 92,
+        cutlery: 12,
+        coffee: 32,
+        bed: 53,
+        car: 25
+      }
+    }, {
+      hoodId: '',
+      similarity: '6.91',
+      parentCity: 'New York',
+      pic: 'assets/img/searchResult/time_s.jpg',
+      desc: 'Times Square is a major commercial intersection and neighborhood in Midtown Manhattan, New York City, at the junction of Broadway and Seventh Avenue, and stretching from West 42nd to West 47th Streets.',
+      data: {
+        beer: 45,
+        cutlery: 34,
+        coffee: 13,
+        bed: 44,
+        car: 16
+      }
+    }, {
+      hoodId: '',
+      similarity: '5.41',
+      parentCity: 'Berlin',
+      pic: 'assets/img/searchResult/Unter_den_Linden.jpg',
+      desc: 'Unter den Linden is a boulevard in the Mitte district of Berlin, the capital of Germany. It is named after its linden trees that line the grassed pedestrian mall between two carriageways.',
+      data: {
+        beer: 23,
+        cutlery: 34,
+        coffee: 84,
+        bed: 23,
+        car: 10
+      }
+    }];
+
+
   }]);
 
 
