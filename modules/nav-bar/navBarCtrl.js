e2decf5f6e75362cf4a37e39cace156de25d9b1c
junipertcy <junipertcy@gmail.com>
2016-12-15 00:36:24 +0800

    temporarily commit

--- modules/nav-bar/navBarCtrl.js
+++ modules/nav-bar/navBarCtrl.js
@@ -8,7 +8,7 @@ NavBar.$inject = ['indexService', 'menuService', 'queryService'];
 function NavBar(indexService, menuService, queryService) {
 	/*jshint validthis: true */
 	var self = this;
-	self.title = "simhood!!";
+	self.title = "Urban Similihood";
 	self.menu = menuService.listMenu();
 
 }
